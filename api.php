87b3e201e2f6fc9fc08a373da04d8b11ed769a98
Sara Romao <sara-romao@live.com>
2024-05-30 23:24:00 -0300

    feat: envio para banco de dados teste

--- api.php
+++ api.php
@@ -10,9 +10,25 @@ if(isset($_POST['dataMensagem'])){
     $email = $mensagem_obj['email_contato'];
     $telefone = $mensagem_obj['telefone_contato'];
     $mensagem = $mensagem_obj['mensagem_contato'];
+    $idCorretor = $mensagem_obj['idCorretor'];
 
     
-   
+    $contato = preg_replace("/[^0-9]/", "", $telefone); 
+    echo $contato;
+
+    
+    $sqlMensagem = "INSERT INTO mensagem (nome_contato, email_contato, telefone_contato, mensagem_contato, data_hora_contato, id_corretor_mensagem) VALUES ('$nome', '$email', '$contato', '$mensagem', NOW(), '$idCorretor')";
+
+     $resultMensagem = mysqli_query($con, $sqlMensagem);
+
+    //  if($resultMensagem){
+
+    //     echo "ok";
+    //  }else{
+    //     echo "erro";
+    //  }
+
+    
 
 }
 
